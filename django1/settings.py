2c8a567881dde538e22b992d819e00c2c6253ec9
unknown <malbuquerquedesouza@gmail.com>
2022-01-04 11:09:29 -0300

    criando aplicação core

--- django1/settings.py
+++ django1/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-2lq2&57$*go81)kn(r%a23v1(1($m2i#&-%)a22%%@cm&1$q(-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+# entrar com o dominio se debug estiver False
 ALLOWED_HOSTS = []
 
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'django1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
