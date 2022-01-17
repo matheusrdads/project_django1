56835510c2db9caa2ef8b75aa9a60d7dd0b22722
unknown <malbuquerquedesouza@gmail.com>
2022-01-17 17:10:09 -0300

    executando a segunda migration

--- core/models.py
+++ core/models.py
@@ -4,3 +4,8 @@ class Produto(models.Model):
     nome = models.CharField('Nome', max_length=100)
     preco = models.DecimalField('Preço', decimal_places=2, max_digits=9)
     estoque = models.IntegerField('Quantidade em estoque')
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('Sobrenome', max_length=100)
+    email = models.EmailField('E-mail', max_length=100)
